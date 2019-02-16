e52559f4c6a52731586ef57502772fc367816427
sarahnash <sarahnash1112@gmail.com>
2019-02-16 13:54:59 -0600

    added minus and change colors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class MyComponent extends React.Component {
     super(props)
     this.state = {
       value: 0,
+      color: '#000000',
     }
   }
   componentDidMount() {
@@ -12,21 +13,35 @@ class MyComponent extends React.Component {
   componentDidUpdate() {
     console.log('this component has changed in props or state')
   }
-  componeWillUnmount() {
+  componentWillUnmount() {
     console.log('this component is being removed from the DOM')
   }
-  handleClick = () => {
+  plusOneClick = () => {
     console.log('you clicked it')
     this.setState({
       value: this.state.value + 1,
     })
   }
+  minusOneClick = () => {
+    console.log('you clicked it')
+    this.setState({
+      value: this.state.value - 1,
+    })
+  }
+  changeColor = () => {
+    this.setState({
+      color: this.state.color = ('#'+Math.floor(Math.random()*16777215).toString(16))
+    })
+    console.log('change color to: ' + this.state.color)
+  }
   render () {
     console.log('This is the render being called', this.props)
     return <div>
       Hello {this.props.name}
-      <h1>{this.state.value}</h1>
-      <button onClick={this.handleClick}>Click me!</button>
+      <h1 style={{ color: this.state.color }}>{this.state.value}</h1>
+      <button onClick={this.plusOneClick}>+1</button>
+      <button onClick={this.minusOneClick}>-1</button>
+      <button onClick={this.changeColor}>Change Color</button>
       </div>
   }
 }
